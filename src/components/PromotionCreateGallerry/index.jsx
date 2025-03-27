7a31ab66b89d271a487cf6078e1bc6c471527e4e
atiqul-islam <imonatikulislam@gmail.com>
2025-03-27 16:57:06 +0600

    Test nothing change

--- src/components/PromotionCreateGallerry/index.jsx
+++ src/components/PromotionCreateGallerry/index.jsx
@@ -3,9 +3,16 @@ import { Link } from "react-router-dom";
 
 const MembershipPromo = () => {
   return (
+    <>
     <div>
       <img src="https://res.cloudinary.com/dgob9antb/image/upload/v1742192987/Become_a_member-3_1_aknpbb.jpg"/>
     </div>
+    <div>
+      <h2>
+        Hello
+      </h2>
+    </div>
+    </>
   )
 };
 
