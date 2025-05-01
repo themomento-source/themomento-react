a4b4cfc8e2622d89067c446a17d6eb9a3e1e46ad
atiqul-islam <imonatikulislam@gmail.com>
2025-05-01 07:38:38 +0600

    Add Firebase auth and recaptcha

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,26 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
-const VITE_FIREBASE_APP_API_KEY = import.meta.env.VITE_FIREBASE_APP_API_KEY;
-const VITE_FIREBASE_APP_AUTH_DOMAIN = import.meta.env
-  .VITE_FIREBASE_APP_AUTH_DOMAIN;
-const VITE_FIREBASE_APP_PROJECT_ID = import.meta.env
-  .VITE_FIREBASE_APP_PROJECT_ID;
-const VITE_FIREBASE_APP_STORAGE_BUCKET = import.meta.env
-  .VITE_FIREBASE_APP_STORAGE_BUCKET;
-const VITE_FIREBASE_APP_MESSAGING_SENDER_ID = import.meta.env
-  .VITE_FIREBASE_MESSAGING_SENDER_ID;
-const VITE_FIREBASE_APP_APP_ID = import.meta.env.VITE_FIREBASE_APP_APP_ID;
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: VITE_FIREBASE_APP_API_KEY,
-  authDomain: VITE_FIREBASE_APP_AUTH_DOMAIN,
-  projectId: VITE_FIREBASE_APP_PROJECT_ID,
-  storageBucket: VITE_FIREBASE_APP_STORAGE_BUCKET,
-  messagingSenderId: VITE_FIREBASE_APP_MESSAGING_SENDER_ID,
-  appId: VITE_FIREBASE_APP_APP_ID,
-};
+export const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_FIREBASE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_APP_ID
+});
 
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+
+const appCheck = initializeAppCheck(firebaseApp, {
+  provider: new ReCaptchaV3Provider('6LctUyorAAAAADy56TcEiUcx54kGNisqT2L-ML59'),
+  isTokenAutoRefreshEnabled: true
+});
